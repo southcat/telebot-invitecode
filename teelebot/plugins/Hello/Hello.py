7f13d96395fa66f00289f439c89ea4cdf8de012e
southcat <32999518+southcat@users.noreply.github.com>
2020-05-23 20:42:05 +0800

    删除多余代码模块，排查了一个莫名其妙的bug

--- teelebot/plugins/Hello/Hello.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-from teelebot import Bot
-
-def Hello(message):
-    #print("你好,世界!")
-    bot = Bot()
-    status = bot.sendPhoto(message["chat"]["id"], bot.plugin_dir + "Hello/helloworld.png")
